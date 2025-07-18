5dbe23a239069b73bf5b0792c46d5e1fdbf9d2c2
dina Muhammed farghly <dinamuhammed2001@gmail.com>
2025-07-18 14:50:50 +0300

    q2

--- oop3/Program.cs
+++ oop3/Program.cs
@@ -30,4 +30,28 @@ public class Calculator
         return a + b;
     }
 }
-#endregion  
+#endregion
+
+# region 2   
+public class Rectangle
+{
+    public int Width { get; set; }
+    public int Height { get; set; }
+    public Rectangle()
+    {
+        Width = 0;
+        Height = 0;
+    }
+    public Rectangle(int width, int height)
+    {
+        Width = width;
+        Height = height;
+    }
+
+    public Rectangle(int size)
+    {
+        Width = size;
+        Height = size;
+    }
+}
+#endregion
