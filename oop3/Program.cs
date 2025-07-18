6910d3af11bd848f7243181d799209a0c42bce30
dina Muhammed farghly <dinamuhammed2001@gmail.com>
2025-07-18 14:58:32 +0300

    q4

--- oop3/Program.cs
+++ oop3/Program.cs
@@ -81,3 +81,21 @@ public class ComplexNumber
     }
 }
 #endregion
+#region 4
+public class Employee
+{
+    public virtual void Work()
+    {
+        Console.WriteLine("Employee is working");
+    }
+}
+
+public class Manager : Employee
+{
+    public override void Work()
+    {
+        base.Work(); 
+        Console.WriteLine("Manager is managing");
+    }
+}
+#endregion
